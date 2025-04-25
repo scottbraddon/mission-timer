8391250d743f9e3b7a86fef779f0b045bcdf2af5
scottbraddon <61992879+scottbraddon@users.noreply.github.com>
2025-04-25 11:01:31 -0600

    Update app-streamlit.py

--- app-streamlit.py
+++ app-streamlit.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Multiplier")
 
 # Inputs
-a = st.number_input("First number", value=0.0, format="%.4f")
+a = st.number_input("First number", value=0.0, step=1)
 b = st.number_input("Second number", value=0.0, format="%.4f")
 
 # Multiply button
